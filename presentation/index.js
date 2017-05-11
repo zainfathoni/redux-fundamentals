76a1e2489aac75061d90c24170d5245065e74d61
Zain Fathoni <zain.fathoni@gmail.com>
2017-05-11 08:22:47 +0700

    Initial commit from spectacle-boilerplate & spectatle-code-slide

---
+++ presentation/index.js
@@ -0,0 +1,107 @@
+// Import React
+import React from "react";
+import CodeSlide from "spectacle-code-slide";
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Text,
+  ComponentPlayground
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+
+
+const images = {
+  city: require("../assets/city.jpg"),
+  kat: require("../assets/kat.png"),
+  logo: require("../assets/formidable-logo.svg"),
+  markdown: require("../assets/markdown.png")
+};
+
+preloader(images);
+
+const theme = createTheme({
+  primary: "white",
+  secondary: "#1F2022",
+  tertiary: "#764ABC",
+  quartenary: "#CECECE"
+}, {
+  primary: "Montserrat",
+  secondary: "Helvetica"
+});
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Spectacle Boilerplate
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            open the presentation/index.js file to get started
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>Typography</Heading>
+          <Heading size={1} textColor="secondary">Heading 1</Heading>
+          <Heading size={2} textColor="secondary">Heading 2</Heading>
+          <Heading size={3} textColor="secondary">Heading 3</Heading>
+          <Heading size={4} textColor="secondary">Heading 4</Heading>
+          <Heading size={5} textColor="secondary">Heading 5</Heading>
+          <Text size={6} textColor="secondary">Standard text</Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <List>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite>Author</Cite>
+          </BlockQuote>
+        </Slide>
+        <CodeSlide
+          transition={[]}
+          lang="js"
+          code={require("raw-loader!../assets/deck.example")}
+          ranges={[
+            { loc: [0, 270], title: "Walking through some code" },
+            { loc: [0, 1], title: "The Beginning" },
+            { loc: [1, 2] },
+            { loc: [1, 2], note: "Heres a note!" },
+            { loc: [2, 3] },
+            { loc: [4, 7], image: images.city },
+            { loc: [8, 10] },
+          ]}
+        />
+          <Slide>
+            <ComponentPlayground
+              code={require("raw-loader!../assets/code.example")}
+            />
+          </Slide>
+      </Deck>
+    );
+  }
+}
