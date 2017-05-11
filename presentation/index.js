b3e1630367d671f2b7ecb78aa9d0e82267ae3ed8
Zain Fathoni <zain.fathoni@gmail.com>
2017-05-11 14:51:23 +0700

    edit: Move sample Slides to the end

--- presentation/index.js
+++ presentation/index.js
@@ -1,6 +1,7 @@
 // Import React
 import React from "react";
 import CodeSlide from "spectacle-code-slide";
+import Interactive from "../assets/interactive";
 
 // Import Spectacle Core tags
 import {
@@ -13,6 +14,7 @@ import {
   Quote,
   Slide,
   Text,
+  Code,
   CodePane,
   ComponentPlayground,
   Link,
@@ -52,52 +54,10 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-        <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
-         {
-          MarkdownSlides`
-#### Create Multiple Slides in Markdown
-All the same tags and elements supported in <Markdown /> are supported in MarkdownSlides.
----
-Slides are separated with **three dashes** and can be used _anywhere_ in the deck. The markdown can either be:
-* A Tagged Template Literal
-* Imported Markdown from another file
-          `
-        }
         <Slide transition={["slide"]} width="100%" >
           <Image width="100%" src={images.pattern}/>
         </Slide>
-        <Slide transition={["slide"]} bgImage={images.pattern.replace("/", "")} bgDarken={0.2}>
+        <Slide transition={["slide"]} bgImage={images.pattern.replace("/", "")} bgDarken={0.5}>
           <Appear fid="1">
             <Heading size={6} caps fit textColor="primary">Props Down</Heading>
           </Appear>
@@ -105,8 +65,13 @@ Slides are separated with **three dashes** and can be used _anywhere_ in the dec
             <Heading size={6} caps fit textColor="primary">Action Up</Heading>
           </Appear>
           <Appear fid="3">
-            <Text textColor="primary" bold>
-              One of <Link href="https://hackernoon.com/10-react-mini-patterns-c1da92f068c5">10 React Mini Patterns</Link>
+            <Text textColor="primary">
+              One of
+              <Link href="https://hackernoon.com/10-react-mini-patterns-c1da92f068c5">
+                <Text textColor="primary" bold>
+                  10 React Mini Patterns
+                </Text>
+              </Link>
             </Text>
           </Appear>
         </Slide>
@@ -155,6 +120,49 @@ Slides are separated with **three dashes** and can be used _anywhere_ in the dec
             code={require("raw-loader!../assets/code.example")}
           />
         </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Spectacle Boilerplate
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            open the presentation/index.js file to get started
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>Typography</Heading>
+          <Heading size={1} textColor="secondary">Heading 1</Heading>
+          <Heading size={2} textColor="secondary">Heading 2</Heading>
+          <Heading size={3} textColor="secondary">Heading 3</Heading>
+          <Heading size={4} textColor="secondary">Heading 4</Heading>
+          <Heading size={5} textColor="secondary">Heading 5</Heading>
+          <Text size={6} textColor="secondary">Standard text</Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <List>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite>Author</Cite>
+          </BlockQuote>
+          <Interactive/>
+        </Slide>
+         {
+          MarkdownSlides`
+#### Create Multiple Slides in Markdown
+All the same tags and elements supported in <Markdown /> are supported in MarkdownSlides.
+---
+Slides are separated with **three dashes** and can be used _anywhere_ in the deck. The markdown can either be:
+* A Tagged Template Literal
+* Imported Markdown from another file
+          `
+        }
       </Deck>
     );
   }
